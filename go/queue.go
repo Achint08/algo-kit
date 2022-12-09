26d95e611f07253133741749ecdd7f0095d95ff8
Achint Sharma <gr8achint.sharma@gmail.com>
2022-12-09 07:19:53 +0000

    Implement queue - Go

---
+++ go/queue.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	queue := list.New()
+
+	queue.PushBack(8)
+	queue.PushBack(6)
+
+	front := queue.Front()
+	fmt.Println(front.Value)
+
+	queue.Remove(front)
+
+	front = queue.Front()
+	fmt.Println(front.Value)
+	
+}
